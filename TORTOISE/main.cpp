006909d5002cde3f25a4e7cf88d66c283db94a76
MarcoBattiato <80609804+MarcoBattiato@users.noreply.github.com>
2022-11-04 09:36:06 +0800

    Initial Commit

---
+++ TORTOISE/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TORTOISE
+//
+//  Created by Marco Battiato on 4/11/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
